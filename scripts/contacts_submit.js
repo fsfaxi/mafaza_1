5a45541037a73039b67d90657018c5f057569ea3
Firas Sfaxi <Firas Sfaxi>
2017-03-17 03:58:54 +0200

    Contact JS Update

--- scripts/contacts_submit.js
+++ scripts/contacts_submit.js
@@ -1,74 +1,97 @@
 
-window.onload = function() {
-    document.getElementById("mf-ct-form-button-submit").disabled = false;
-}
-// Create the XHR object.
-function createCORSRequest(method, url) 
-{
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-        // XHR for Chrome/Firefox/Opera/Safari.
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined") {
-        // XDomainRequest for IE.
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        // CORS not supported.
-        xhr = null;
-    }
-    return xhr;
-}
 
-// Make the actual CORS request.
-function makeCorsRequest(jsonObject) 
-{
-    // This is a sample server that supports CORS.
-    var url = 'http://milllymailer.azurewebsites.net/api/Submit/2';
+window.addEventListener("load",function(){
+     if(document.getElementById("mf-ct-form-button-submit"))
+        document.getElementById("mf-ct-form-button-submit").disabled = false;
+});
 
-    var xhr = createCORSRequest('POST', url);
-    if (!xhr) {
-        alert('CORS not supported');
-        return;
-    }
+(function(){
 
-    xhr.onerror = function(e) {
-        console.dir(e);
-        alert('Woops, there was an error making the request.');
-    };
+        var contact_onload = function(){
 
-    // JSON message goes here
-    // var requestBody = {"firstName":"firas","surname":"sfaxi","email":"f.sfaxi@almutmaina.com","tel":"+218944013886"};
-    var bodyJSON=JSON.stringify(jsonObject);
-    xhr.setRequestHeader(
-    'Content-Type', 'application/json');
-    xhr.send(bodyJSON);
-}
+                    // Create the XHR object.
+                    function createCORSRequest(method, url) 
+                    {
+                        var xhr = new XMLHttpRequest();
+                        if ("withCredentials" in xhr) {
+                            // XHR for Chrome/Firefox/Opera/Safari.
+                            xhr.open(method, url, true);
+                        } else if (typeof XDomainRequest != "undefined") {
+                            // XDomainRequest for IE.
+                            xhr = new XDomainRequest();
+                            xhr.open(method, url);
+                        } else {
+                            // CORS not supported.
+                            xhr = null;
+                        }
+                        return xhr;
+                    }
+
+                    // Make the actual CORS request.
+                    function makeCorsRequest(jsonObject) 
+                    {
+                        // This is a sample server that supports CORS.
+                        var url = 'http://milllymailer.azurewebsites.net/api/Submit/2';
+
+                        var xhr = createCORSRequest('POST', url);
+                        if (!xhr) {
+                            alert('CORS not supported');
+                            return;
+                        }
+
+                        xhr.onerror = function(e) {
+                            console.dir(e);
+                            alert('Woops, there was an error making the request.');
+                        };
+
+                        // JSON message goes here
+                        // var requestBody = {"firstName":"firas","surname":"sfaxi","email":"f.sfaxi@almutmaina.com","tel":"+218944013886"};
+                        var bodyJSON=JSON.stringify(jsonObject);
+                        xhr.setRequestHeader(
+                        'Content-Type', 'application/json');
+                        xhr.send(bodyJSON);
+                    }
+
+                    function printFormData (jsonData){
+
+                        for(var pair of jsonData.entries()) {
+                            console.log(pair[0]+ ', '+ pair[1]); 
+                        }
+
+                    }
+
+                    function formDataToJSON (formData){
+
+                        var jsonData = {};
+
+                        for(var value of formData.entries()){
+
+                            jsonData[value[0]] = value[1];
+
+                        }
+
+                        console.log(jsonData);
+                        return jsonData;
+
+                    }
+
+                    if(document.getElementById("mfz-ct-form-container")){ 
 
 
-if(document.getElementById("mfz-ct-form-container")){
+                            var form = document.getElementById("mfz-ct-form-container");
+                            
+                            form.addEventListener('submit',function(event){
 
+                                event.preventDefault();
+                                var formData = new FormData(form);
 
-        var form = document.getElementById("mfz-ct-form-container");
+                                var jsonData = formDataToJSON(formData);
+                                makeCorsRequest(jsonData);
+                                document.getElementById("mf-ct-form-button-submit").disabled = true;
+                                
+                                });
+                    }
+            }  
 
-        form.addEventListener('submit',function(event){
-
-            event.preventDefault();
-            var jsonData = {
-                "name":this.name.value,
-                "telephone":this.telephone.value,
-                "Email":this.email.value,
-                "Company":this.company.value,
-                "message":this.message.value
-            };
-
-            console.log(jsonData);
-            makeCorsRequest(jsonData);
-            document.getElementById("mf-ct-form-button-submit").disabled = true;
-
-            
-            });
-}
-
-
-
+            window.addEventListener("load", contact_onload); 
+})();
--- scripts/map.js
+++ scripts/map.js
@@ -1,39 +1,40 @@
 
 (function(){
-if (document.querySelector("[data-map-container]")){
-    var runMap = function (){
-
-        var mapContainer = document.querySelector("[data-map-container]");
-        var map = mapContainer.querySelector("[data-map]");
-        var mapClose = mapContainer.querySelector("[data-map-close]");
-        var body = document.body;
         
-        mapContainer.style.visibility = 'visible';
-        window.scrollTo(0,0);
-        body.style.overflow = 'hidden';
-        
-        var myCenter = new google.maps.LatLng(map.dataset.mapLat,map.dataset.mapLng);
-        var mapCanvas = document.getElementById("mfz-contact-map-innercontainer");
-        var mapOptions = {center: myCenter, zoom: 15};
-        var map = new google.maps.Map(mapCanvas, mapOptions);
-        var marker = new google.maps.Marker({position:myCenter});
-        marker.setMap(map);
+    if (document.querySelector("[data-map-container]")){
+        var runMap = function (){
 
-        var closeMap = function (){
+            var mapContainer = document.querySelector("[data-map-container]");
+            var map = mapContainer.querySelector("[data-map]");
+            var mapClose = mapContainer.querySelector("[data-map-close]");
+            var body = document.body;
             
-            mapContainer.style.visibility = 'hidden';
-            body.style.overflow = 'visible';
+            mapContainer.style.visibility = 'visible';
+            window.scrollTo(0,0);
+            body.style.overflow = 'hidden';
+            
+            var myCenter = new google.maps.LatLng(map.dataset.mapLat,map.dataset.mapLng);
+            var mapCanvas = document.getElementById("mfz-contact-map-innercontainer");
+            var mapOptions = {center: myCenter, zoom: 15};
+            var map = new google.maps.Map(mapCanvas, mapOptions);
+            var marker = new google.maps.Marker({position:myCenter});
+            marker.setMap(map);
 
-         }
+            var closeMap = function (){
+                
+                mapContainer.style.visibility = 'hidden';
+                body.style.overflow = 'visible';
 
-         mapClose.addEventListener("click", closeMap);
+            }
 
+            mapClose.addEventListener("click", closeMap);
+
+        }
+
+        window.addEventListener("load", function(){
+            var mapImg = document.querySelector("[data-map-img]");
+            mapImg.addEventListener("click", runMap);
+        });
+        
     }
-
-     window.addEventListener("load", function(){
-         var mapImg = document.querySelector("[data-map-img]");
-         mapImg.addEventListener("click", runMap);
-     });
-    
-}
 })();
